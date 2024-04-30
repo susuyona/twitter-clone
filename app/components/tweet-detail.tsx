f7249ff4fc220b14a05702ecf9f6d4105e91cfa5
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-04-30 21:29:24 +0200

    feat: like button for tweets

--- app/components/tweet-detail.tsx
+++ app/components/tweet-detail.tsx
@@ -1,37 +1,47 @@
 import { formatToTimeAgo } from "@/lib/utils";
 import { Tweet } from "@/lib/constants";
+import {
+  checkLikedState,
+  toggleLike as toggleLikeAction,
+} from "../tweet/[id]/actions";
+import { useEffect, useState } from "react";
 
-export default function TweetInDetail({
-  id,
-  text,
-  likes,
-  createdAt,
-  authorId,
-  author,
-  handleLike,
-  likedMessage,
-}: Tweet & { handleLike: () => void; likedMessage: boolean }) {
+export default function TweetInDetail(tweet: Tweet) {
+  const [liked, setLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    checkLikedState(tweet.id).then((isLiked) => setLiked(isLiked));
+  }, [liked]);
+
+  function toggleLike(tweetId: number) {
+    toggleLikeAction(tweetId)
+      .then((isLiked) => setLiked(isLiked))
+      .catch((error) => console.error("Error toggling like:", error));
+  }
+
   return (
     <div
       className="flex flex-col justify-center items-center max-w-sm mx-auto 
       gap-2 mb-5 p-5 rounded-xl border-[1px] border-black"
     >
       <div>
-        <span>{author.username}</span>
+        <span>{tweet.author.username}</span>
       </div>
       <div>
-        <span>{text}</span>
+        <span>{tweet.text}</span>
       </div>
       <div>
-        <span className="text-xs">{formatToTimeAgo(createdAt.toString())}</span>
+        <span className="text-xs">
+          {formatToTimeAgo(tweet.createdAt.toString())}
+        </span>
       </div>
       <div>
         <button
-          onChange={handleLike}
+          onClick={() => toggleLike(tweet.id)}
           className="bg-orange-200 rounded-md text-black font-medium text-center
         hover:bg-orange-400 transition-colors px-2 py-1"
         >
-          {likedMessage ? "💔" : "💖"}
+          {liked ? "💔" : "💖"}
         </button>
       </div>
     </div>
--- app/tweet/[id]/page.tsx
+++ app/tweet/[id]/page.tsx
@@ -5,46 +5,21 @@ import Button from "@/app/components/button";
 import Header from "@/app/components/header";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { getTweet, toggleLike, checkLikedState } from "./actions";
+import { getTweet } from "./actions";
 import TweetInDetail from "@/app/components/tweet-detail";
 import { Tweet } from "@/lib/constants";
 
 export default function TweetDetail({ params }: { params: { id: string } }) {
   const id = Number(params.id);
-  const [tweet, setTweet] = useState<Tweet | null>(null);
-
+  const [tweet, setTweet] = useState<Tweet>();
   useEffect(() => {
-    const fetchTweet = () => {
-      getTweet(id)
-        .then((tweetData) => {
-          if (!tweetData) {
-            return notFound();
-          } else {
-            setTweet(tweetData);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching tweet:", error);
-        });
-    };
-    fetchTweet();
-  }, []);
-
-  const [liked, setLiked] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchLike = () => {
-      checkLikedState(id).then((isLiked) => {
-        setLiked(isLiked);
-      });
+    try {
+      getTweet(id).then((tweet) => setTweet(tweet));
+    } catch (error) {
+      console.error("Error fetching tweet:", error);
+      notFound();
     }
-    fetchLike();
-  }, [tweet]);
-
-  const handleLike = () => {
-    toggleLike(tweet!.id);
-    setLiked(!liked);
-  };
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center max-w-sm mx-auto gap-5 m-5">
@@ -52,13 +27,7 @@ export default function TweetDetail({ params }: { params: { id: string } }) {
       <Link href="/">
         <Button text="Back to the list" />
       </Link>
-      {tweet && (
-        <TweetInDetail
-          {...tweet}
-          handleLike={handleLike}
-          likedMessage={liked}
-        />
-      )}
+      {tweet && <TweetInDetail {...tweet} />}
     </div>
   );
 }
